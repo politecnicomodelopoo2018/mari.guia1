43cc9099fc5f8be66887942556e3974b207f6fc1
mariireynoso <fernando6reynoso@hotmail.com>
2018-03-23 11:13:39 -0300

    Ejer3 clase equipo

---
+++ e3_equipo.py
@@ -0,0 +1,17 @@
+from ejer3 import Campeonato
+
+c= Campeonato()
+
+class Equipo(object):
+    nombre_equipo=None
+    nombre_jugador=None
+    barrio_procedencia=None
+
+    def __init__(self,nombre_equipo=None):
+        self.nombre_equipo=[]
+
+    def __init__(self,nombre_jugador=None):
+        self.nombre_jugador=[]
+
+    def __init__ (self,barrio_procedencia=None)
+        self.barrio_procedencia=[]
