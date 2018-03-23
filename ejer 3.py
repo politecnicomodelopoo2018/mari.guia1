f5425c6abe4b0b52e44101745729725e8a898146
mariireynoso <fernando6reynoso@hotmail.com>
2018-03-23 11:14:11 -0300

    Ejer3 clase campeonato

---
+++ ejer 3.py
@@ -0,0 +1,11 @@
+class Campeonato(object):
+    jugador:None
+    turnos:None
+
+    def __init__(self,jugador=None):
+        self.jugador=jugador
+
+    def__init__(self,turnos=None):
+        self.turnos=turnos[]
+        self.dias=dias[]
+
