c2543a693d378f19adb937c0658502d3520c0f8c
mariireynoso <fernando6reynoso@hotmail.com>
2018-03-23 08:54:41 -0300

    Ejer:4 prints

--- mainej4.py
+++ mainej4.py
@@ -1,17 +1,21 @@
 from clase_empleado import Empresa
 from ejer4 import Empleado
 import datetime
+import calendar
 
+c = calendar.HTMLCalendar(calendar.SUNDAY)
+print c.formatmonth(2007, 7)
 
 a= Empresa()
-empleado =  Empleado()
+empleado =Empleado()
 
 empleado.nombre= input ("ingrese nombre ")
 empleado.apellido=input ("ingrese apellido ")
 empleado.telefono= input ("ingrese telefono ")
 empleado.fecha_nac= datetime.date(2000,10,23)
+por = empleado.Porcentaje()
+print(por)
 
-print(empleado.Porcentaje())
 
 
 
