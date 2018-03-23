0ea24e892114f23e60606d72b11aadfd69302282
mariireynoso <fernando6reynoso@hotmail.com>
2018-03-23 07:58:48 -0300

    hola es la tercera

--- clase_empleado.py
+++ clase_empleado.py
@@ -1,14 +1,14 @@
 from ejer4 import Empleado
-import datetime
 
 class Empresa(object):
 
     def __init__(self):
         self.empleados = []
 
-     a=Empleado()
+    a=Empleado()
+
 
     def AgregarEmpleados(self,empleados):
-
+        self.empelados.append(empleados)
 
 
--- mainej4.py
+++ mainej4.py
@@ -1,6 +1,18 @@
-from ejer4 import Empresa
+from clase_empleado import Empresa
+from ejer4 import Empleado
 import datetime
 
 
-a=Empresa()
-print ("Ingrese un empleado")
+a= Empresa()
+empleado =  Empleado()
+
+empleado.nombre= input ("ingrese nombre ")
+empleado.apellido=input ("ingrese apellido ")
+empleado.telefono= input ("ingrese telefono ")
+empleado.fecha_nac= datetime.date(2000,10,23)
+
+print(empleado.Porcentaje())
+
+
+
+
