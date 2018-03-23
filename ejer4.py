54fb2f4ff7cf5fba6306e4d1cbafa2606cab857a
mariireynoso <fernando6reynoso@hotmail.com>
2018-03-23 10:00:34 -0300

    Ejer:4 prints

--- ejer4.py
+++ ejer4.py
@@ -1,3 +1,5 @@
+import calendar
+
 class Empleado (object):
     nombre=None
     apellido=None
@@ -27,14 +29,25 @@ class Empleado (object):
             if dias_que_debe_ir[item] == True:
                 self.dias_concurridos.append(dias_que_debe_ir)
 
-    def DiasQueDebeir (self):
-        self.dias_que_debe_ir = [False,True,True,True,True,True,False]
+    def DiasQueDebeir (self,año,mes):
+        cdias_que_fue=0
+        cdias_q_tiene_q_ir=0
+        cant_dias_mes=calendar.monthrange(año,mes)
+        for i in range (1,cant_dias_mes+1):
+            fecha=datetime.date(año,mes,i)
+            if fecha==item:
+                cdias_que_fue +=1
+                break
+            cdias_q_tiene_q_ir += 1
+        return cdias_que_fue/cdias_q_tiene_q_ir
 
     def Porcentaje (self):
         suma=0
         for item in self.empleados:
             suma+=item.porcentaje(año,mes)
         return suma/len(self.empleados)
+
+
 
 
 
