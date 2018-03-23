c506a99a8a01acac48042ab8a3da2ce83cc81e48
mariireynoso <fernando6reynoso@hotmail.com>
2018-03-23 08:53:27 -0300

    Ejer:4

--- clase_empleado.py
+++ clase_empleado.py
@@ -1,14 +1,25 @@
 from ejer4 import Empleado
+import calendar
+
+c = calendar.HTMLCalendar(calendar.SUNDAY)
+print c.formatmonth(self,)
 
 class Empresa(object):
+    nombre=None
 
-    def __init__(self):
+    def __init__(self,nombre=None):
+        self.nombre=nombre
         self.empleados = []
 
     a=Empleado()
 
-
     def AgregarEmpleados(self,empleados):
         self.empelados.append(empleados)
 
+    def buscarEmpleado(self,nombre):
+        for item in self empleados:
+            if item.nombre
+                return item
+        return None
 
+    def porcentaje
